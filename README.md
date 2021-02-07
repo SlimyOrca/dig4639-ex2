c63354924a570f109b029cd81f700d3f7f505fc7
SlimyOrca <tuckeringram8@gmail.com>
2021-02-07 13:08:58 -0500

    created quote app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dig4639-ex2
+
+"We all poop the same color"
+- Tucker Ingram
---
+++ index.js
@@ -0,0 +1,10 @@
+let quote = [`
+"We all poop the same color"
+- Tucker Ingram`,
+`"Art is love made public"
+- Hernando, from Sense8`,
+`"Theres nothing more frightening then having a dream come true"
+- Hernando, from Sense8`,
+]
+let index = Math.floor(Math.random() * 3);
+console.log(quote[index]);
